90f8db3b673657383692a9f60e6585f0254e67be
aliuppal <aliuppal@gmail.com>
2021-06-19 17:05:57 +0500

    class 3 part 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Dinner from './dinner';
 
 function App() {
   return (
-    <div>
-     Hello World!!
-    </div>
-  );
+    <div className="App">
+   <Dinner dishName="Karahi" sweetDish="Kheer"/>
+   <Dinner dishName="Biryani" sweetDish="Halwa"/>
+   <Dinner dishName="Shawarma" sweetDish="Ice Cream"/>
+   </div>
+  )
 }
 
 export default App;
